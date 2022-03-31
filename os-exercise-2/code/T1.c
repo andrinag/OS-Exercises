0220f79b61342c37247a0156e0b6717c0e740ea2
Tobias Hafner <Tobias_Hafner@live.de>
2022-03-31 18:09:54 +0200

    wrote stuff in T1.c

--- os-exercise-2/code/T1.c
+++ os-exercise-2/code/T1.c
@@ -19,36 +19,57 @@ int main ()
 			arr[(SIZE-1)-i]=0;
 		else
 			arr[(SIZE-1)-i]=1;
-	sortedArr = sort(arr, SIZE);
+	sort(arr, SIZE);
 
-	// TODO fork another process here
-	pid = fork()
-	// One process should calculate the median
+	// create shared memory
+	errno = 0;
+	int file_desc = shm_open("/reg", O_CREAT | O_RDWR, S_IRUSR | S_IWUSR);
+	if (file_desc < 0) {
+		return errno;
+	}
+	// truncate to length of one float
+	if (ftruncate(file_desc, sizeof(float)) < 0) {
+		return -1;
+	}
+	// map memory
+	float* reg_ptr = mmap(NULL, sizeof(float), PROT_READ | PROT_WRITE, MAP_SHARED, file_desc, 0);
+	if (reg_ptr = MAP_FAILED) {
+		return -1;
+	}
+
+	// fork another process here
+	int pid = fork();
+	if (pid < 0) {
+		return -1;
+	}
+
+	// parent calculates the mean, the child calculates the median
+	float mean = 0;
 	if (fork == 0) {
 		// child
 		// calculate median value
-		float median = arr[(SIZE-1)/2];
+		*reg_ptr = arr[(SIZE-1)/2];
+
+		// one process should communicate its value to the other process
+
 	} else {
 		// parent
 		// calculate mean value
-		float mean = 0;
 		for (int i = 0; i < SIZE-1; i++) {
 			mean += arr[i];
 		}
 		mean /= SIZE;
 	}
-	// One process should communicate its value to the other process
+
 	// The other process should print both calculated results
-	printf("The child with id %d calculated a median of %f", pid, median);
+	printf("The child with id %d calculated a median of %f", pid, *reg_ptr);
 	printf("The parent calculated a mean of %f", mean);
 
-
-	// Hint use shm_open, ftrucate, mmap, and mumap to communicate over mapped files
+	// unmap memory
+	munmap(reg_ptr, sizeof(float));
 
 	return 0;
 }
-
-
 
 
 void swap(int *xp, int *yp)
