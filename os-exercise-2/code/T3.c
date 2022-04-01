0bb872f31276025d53cc0a7b8f27dc75a03ca57a
Tobias Hafner <Tobias_Hafner@live.de>
2022-04-01 10:38:13 +0200

    add ex2 T3.c

--- os-exercise-2/code/T3.c
+++ os-exercise-2/code/T3.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 #include<stdlib.h>
-#include<unistd.h> 
-#include<pthread.h> 
+#include<unistd.h>
+#include<pthread.h>
 #define SIZE 10000
 #define NUM_THREADS 5
 int result;
@@ -9,30 +9,60 @@ pthread_mutex_t lock;
 
 struct args
 {
-	int *arr;
+	int* arr;
 	int start;
 	int end;
 };
 
 void * accumulate(void * in)
 {
-	//TODO implement accumlate
+	struct args* targs = (struct args *) in;
+	int sum = 0;
+	for (int i = targs->start; i < targs->end; i++) {
+		sum += targs->arr[i];
+	}
+	printf("i'm a thread and got %i\n", sum);
+	while(pthread_mutex_trylock(&lock)!=0);
+	result += sum;
+	pthread_mutex_unlock(&lock);
 	return NULL;
 }
-int main ()
-{
 
+int main () {
 	result=0;
-        int arr[SIZE];    
+        int arr[SIZE];
         for(int i=0;i<SIZE;i++)
 		arr[i]=1;
 
 	pthread_t threads[NUM_THREADS-1];
 	struct args thread_args[NUM_THREADS-1];
-	// TODO create a team of thread, each thread must take SIZE/NUM_THREADS to accumulate
-	// TODO use struct args to pass arguments to the accumulate function  
-	// TODO main thread must participate in the calculation
-	// TODO make sure all threads finised
+
+	// create a team of thread, each thread must take SIZE/NUM_THREADS to accumulate
+	for (int i = 1; i < NUM_THREADS; i++) {
+                // set values of args struct
+		thread_args[i].arr = arr;
+		thread_args[i].start = (SIZE / NUM_THREADS) * i;
+		thread_args[i].end = ((SIZE / NUM_THREADS) * (i+1));
+		//create thread
+		pthread_create(&threads[i], NULL, accumulate,&thread_args[i]);
+        }
+
+	// main thread must participate in the calculation
+	int sum = 0;
+	for (int i = 0; i < (SIZE / NUM_THREADS); i++) {
+		sum += arr[i];
+		//printf("index is %d::::: end is %f", i, SIZE / NUM_THREADS);
+	}
+	printf("i'm the main thread and got %i\n", sum);
+	while(pthread_mutex_trylock(&lock)!=0);
+        result += sum;
+        pthread_mutex_unlock(&lock);
+
+	//make sure all threads finised
+	for (int i = 1; i < NUM_THREADS; i++) {
+                //create thread
+                pthread_join(threads[i], NULL);
+        }
 	printf("sum  is %d\n", result);
 }
 
