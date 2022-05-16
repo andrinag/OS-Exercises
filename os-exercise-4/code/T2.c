d9e31c67955a4b708fbc03a03e0cd132b0bcc510
ruben.hutter <ruben.hutter@stud.unibas.ch>
2022-05-16 20:54:14 +0200

    implemented exercise 2

--- os-exercise-4/code/T2.c
+++ os-exercise-4/code/T2.c
@@ -1,12 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int readLinesCount(const char * str){
-    //TODO implement readlinesCount which returns the total number of lines within the input file
- return 0;
+int readLinesCount(const char * str) {
+	//TODO implement readlinesCount which returns the total number of lines within the input file
+	FILE *fp = fopen(str, "r");
+	int ch = 0;
+	int lines = 0;
+	while (!feof(fp)) {
+		ch = fgetc(fp);
+		if (ch == '\n') {
+			lines++;
+		}
+	}
+	fclose(fp);
+	return lines;
 }
-void writeLinesCount(const char * str, int count_line){
-//TODO implement writeLinesCount which writes a new line in file str has count_line 
+void writeLinesCount(const char * str, int count_line) {
+	//TODO implement writeLinesCount which writes a new line in file str has count_line
+	FILE *fp = fopen(str, "a");
+	char str_count[10];
+	sprintf(str_count, "%d\n", count_line);
+	fprintf(fp, str_count);
+	fclose(fp);
 }
 int main(int argc, char * argv[])
 {
